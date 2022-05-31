19baadd95f0c616e22b415912890644dd3e9220e
Alessandro Scandone <alescandone@gmail.com>
2022-05-31 04:24:13 +0200

    added quotation sugare

--- src/main/scala/value/parser/Parser.scala
+++ src/main/scala/value/parser/Parser.scala
@@ -13,11 +13,18 @@ object Parser extends RegexParsers {
   // TODO reuse rs
     """[a-zA-Z&_+<>\-*!/][a-zA-Z&_+<>\-*!/0-9]*""".r ^^ Symbol[Nothing]
 
-  private def expr: Parser[List[Nothing]] = ("(" ~> rep(value) <~ ")") ^^ List[Nothing]
+  private def form: Parser[List[Nothing]] = ("(" ~> rep(value) <~ ")") ^^ List[Nothing]
 
-  private def value: Parser[Value[Nothing]] = numberLiteral | stringLiteral | symbol | expr
+  private def value: Parser[Value[Nothing]] =
+    numberLiteral | stringLiteral | symbol | quotationSugar | form
 
   private def numberLiteral: Parser[Number[Nothing]] = """[1-9][0-9]*|0""".r ^^ { value => Number(value.toFloat) }
 
   private def stringLiteral: Parser[String[Nothing]] = "\"" ~> """[a-zA-Z0-9:*/+\- !]*""".r <~ "\"" ^^ String[Nothing]
+
+  private def quotationSugar: Parser[Value[Nothing]] =
+    quotedValue("'", "quote") | quotedValue("`", "backquote") | quotedValue(",", "unquote") | quotedValue(",@", "unquote-splicing")
+
+  private def quotedValue(token: java.lang.String, name: java.lang.String): Parser[Value[Nothing]] =
+    token ~> value ^^ { value => List.of(Symbol(name), value) }
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -6,44 +6,71 @@ import value.parser.Parser
 class ParserSpec extends AnyFlatSpec with should.Matchers {
 
   it should "parse symbols" in {
-    Parser.run("abc").get should be(
-      scala.List(Symbol("abc")))
+    Parser.run("abc").get.head should be(Symbol("abc"))
   }
 
   it should "parse int numbers" in {
-    Parser.run("23").get should be(
-      scala.List(Number(23)))
+    Parser.run("23").get.head should be(Number(23))
   }
 
   it should "parse string literals" in {
-    Parser.run("\"abc\"").get should be(
-      scala.List(String("abc")))
+    Parser.run("\"abc\"").get.head should be(String("abc"))
   }
 
   it should "parse the empty list" in {
-    Parser.run("()").get should be(
-      scala.List(List()))
+    Parser.run("()").get.head should be(List())
   }
 
   it should "parse a nonempty list" in {
-    Parser.run("(1 2 3)").get should be(
-      scala.List(List[Nothing](scala.List(1, 2, 3))))
+    Parser.run("(1 2 3)").get.head should be(List.of(1, 2, 3))
   }
 
-  it should "parse a nested lists" in {
-    Parser.run("(1 () (1 2) 3)").get should be(
-      scala.List[Value[Nothing]](
-        List.of(
-          1,
-          List.of(),
-          List.of(1, 2),
-          3)
-      )
+  it should "parse nested lists" in {
+    Parser.run("(1 () (1 2) 3)").get.head should be(
+      List.of(1, Nil, List.of(1, 2), 3)
     )
   }
 
   it should "parse multiple expressions" in {
     Parser.run("a b c").get should be(
       scala.List(Symbol("a"), Symbol("b"), Symbol("c")))
+  }
+
+  it should "handle quote sugar" in {
+    Parser.run("'x").get.head should be(List.of(
+      Symbol("quote"),
+      Symbol("x"),
+    ))
+
+    Parser.run("'  x").get.head should be(List.of(
+      Symbol("quote"),
+      Symbol("x"),
+    ))
+
+    Parser.run("'  ()").get.head should be(List.of(
+      Symbol("quote"),
+      Nil,
+    ))
+  }
+
+  it should "handle backquote sugar" in {
+    Parser.run("`x").get.head should be(List.of(
+      Symbol("backquote"),
+      Symbol("x"),
+    ))
+  }
+
+  it should "handle unquote sugar" in {
+    Parser.run(",x").get.head should be(List.of(
+      Symbol("unquote"),
+      Symbol("x"),
+    ))
+  }
+
+  it should "handle unquote splicing sugar" in {
+    Parser.run(",@x").get.head should be(List.of(
+      Symbol("unquote-splicing"),
+      Symbol("x"),
+    ))
   }
 }
