17ce5b9403abcd2121ae60430161bbbd6073975d
ascandone <alescandone@gmail.com>
2022-06-25 11:30:48 +0200

    more ergonomic assertion

--- src/test/scala/compiler/CompilerSpec.scala
+++ src/test/scala/compiler/CompilerSpec.scala
@@ -5,143 +5,116 @@ import org.scalatest.matchers._
 import value._
 import value.parser.Parser
 import vm.opcode._
+import scala.language.implicitConversions
 
 class CompilerSpec extends AnyFlatSpec with should.Matchers {
 
   it should "compile constants" in {
-    testCompileAs("42",
-      Array(
-        Push(42)
-      )
+    "42".shouldCompileAs(
+      Push(42)
     )
 
-    testCompileAs("\"hello\"",
-      Array(
-        Push("hello")
-      )
+    "\"hello\"".shouldCompileAs(
+      Push("hello")
     )
 
-    testCompileAs("()",
-      Array(
-        Push(Nil)
-      )
+    "()".shouldCompileAs(
+      Push(Nil)
     )
 
-    testCompileAs("true",
-      Array(
-        Push(Symbol("true"))
-      )
+    "true".shouldCompileAs(
+      Push(Symbol("true"))
     )
 
-    testCompileAs("false",
-      Array(
-        Push(Symbol("false"))
-      )
+    "false".shouldCompileAs(
+      Push(Symbol("false"))
     )
   }
 
   it should "compile empty do blocks" in {
-    testCompileAs("(do)",
-      Array(
-        Push(Nil)
-      )
+    "(do)".shouldCompileAs(
+      Push(Nil)
     )
   }
 
   it should "compile singleton do blocks" in {
-    testCompileAs("(do 42)",
-      Array(
-        Push(42),
-      )
+    "(do 42)".shouldCompileAs(
+      Push(42),
     )
   }
 
   it should "compile do blocks with many values" in {
-    testCompileAs("(do 1 2 3)",
-      Array(
-        Push(1),
-        Pop,
-        Push(2),
-        Pop,
-        Push(3),
-      )
+    "(do 1 2 3)".shouldCompileAs(
+      Push(1),
+      Pop,
+      Push(2),
+      Pop,
+      Push(3),
     )
   }
 
   it should "treat multiple values as a do block" in {
-    testCompileAs("1 2 3",
-      Array(
-        Push(1),
-        Pop,
-        Push(2),
-        Pop,
-        Push(3),
-      )
+    "1 2 3".shouldCompileAs(
+      Push(1),
+      Pop,
+      Push(2),
+      Pop,
+      Push(3),
     )
   }
 
   it should "compile +" in {
-    testCompileAs("(builtin/add 1 2)",
-      Array(
-        Push(1),
-        Push(2),
-        Add,
-      )
+    "(builtin/add 1 2)".shouldCompileAs(
+      Push(1),
+      Push(2),
+      Add,
     )
   }
 
   it should "compile >" in {
-    testCompileAs("(builtin/greater-than 1 2)",
-      Array(
-        Push(1),
-        Push(2),
-        GreaterThan,
-      )
+    "(builtin/greater-than 1 2)".shouldCompileAs(
+      Push(1),
+      Push(2),
+      GreaterThan,
     )
   }
 
   it should "compile not" in {
-    testCompileAs("(builtin/not true)",
-      Array(
-        Push(true),
-        Not,
-      )
+    "(builtin/not true)".shouldCompileAs(
+      Push(true),
+      Not,
     )
   }
 
   it should "compile a if expression of values" in {
-    testCompileAs("(if true \"when true\" \"when false\")",
-      Array(
-        /* 0 */ Push(true),
-        /* 1 */ JumpIfNot(4),
-        /* 2 */ Push("when true"),
-        /* 3 */ Jump(5),
-        /* 4 */ Push("when false"),
-        /* 5 */
-      )
+    "(if true \"when true\" \"when false\")".shouldCompileAs(
+      /* 0 */ Push(true),
+      /* 1 */ JumpIfNot(4),
+      /* 2 */ Push("when true"),
+      /* 3 */ Jump(5),
+      /* 4 */ Push("when false"),
+      /* 5 */
     )
   }
 
   it should "compile a if expression of expressions" in {
-    testCompileAs("(if (builtin/greater-than 100 200) (builtin/add 10 20) (builtin/not ()))",
-      Array(
-        /* 00 */ Push(100),
-        /* 01 */ Push(200),
-        /* 02 */ GreaterThan,
-        /* 03 */ JumpIfNot(8),
-        /* 04 */ Push(10), // if branch
-        /* 05 */ Push(20),
-        /* 06 */ Add,
-        /* 07 */ Jump(10),
-        /* 08 */ Push(List.of()), // else branch
-        /* 09 */ Not, // else branch
-        /* 10 */
-      )
+    "(if (builtin/greater-than 100 200) (builtin/add 10 20) (builtin/not ()))".shouldCompileAs(
+      /* 00 */ Push(100),
+      /* 01 */ Push(200),
+      /* 02 */ GreaterThan,
+      /* 03 */ JumpIfNot(8),
+      /* 04 */ Push(10), // if branch
+      /* 05 */ Push(20),
+      /* 06 */ Add,
+      /* 07 */ Jump(10),
+      /* 08 */ Push(List.of()), // else branch
+      /* 09 */ Not, // else branch
+      /* 10 */
     )
   }
 
   it should "compile def expressions" in {
-    testCompileAs("(def x 10) (def y 20) (builtin/add x y)", Array(
+    "(def x 10) (def y 20) (builtin/add x y)".shouldCompileAs(
       Push(10),
       SetGlobal(0),
       Pop,
@@ -153,118 +126,118 @@ class CompilerSpec extends AnyFlatSpec with should.Matchers {
       GetGlobal(0),
       GetGlobal(1),
       Add,
-    ))
+    )
   }
 
   behavior of "quote special form"
   it should "be a noop with literals" in {
-    testCompileAs("'42", Array(
+    "'42".shouldCompileAs(
       Push(42),
-    ))
+    )
 
-    testCompileAs("'\"hello\"", Array(
+    "'\"hello\"".shouldCompileAs(
       Push("hello"),
-    ))
+    )
 
-    testCompileAs("'()", Array(
+    "'()".shouldCompileAs(
       Push(List()),
-    ))
+    )
   }
 
   it should "prevent symbols from evaluating" in {
-    testCompileAs("'a", Array(
+    "'a".shouldCompileAs(
       Push(Symbol("a")),
-    ))
+    )
   }
 
   it should "prevent lists from evaluating" in {
-    testCompileAs("'(a b)", Array(
+    "'(a b)".shouldCompileAs(
       Push(List.of(Symbol("a"), Symbol("b"))),
-    ))
+    )
   }
 
   behavior of "macros"
   they should "be evaluated as nil when defined" in {
-    testCompileAs("(defmacro mac () ())", Array(
+    "(defmacro mac () ())".shouldCompileAs(
       Push(Nil),
-    ))
-  }
-
-  they should "be expanded when returning literals" in {
-    testCompileAs("(defmacro mac () 42) (mac)", Array(
-      Push(Nil),
-      Pop,
-      Push(42),
-    ))
-  }
-
-  they should "be expanded when returning expressions" in {
-    testCompileAs("(def x 42) (defmacro mac () (quote x)) (mac)", Array(
-      Push(42),
-      SetGlobal(0),
-      Pop,
-
-      Push(Nil),
-      Pop,
-
-      GetGlobal(0),
-    ))
-  }
-
-  they should "have access to arguments" in {
-    testCompileAs("(defmacro mac (x y) x) (mac 42 1)", Array(
-      Push(Nil),
-      Pop,
-
-      Push(42),
-    ))
-  }
-
-  they should "prevent arguments to evaluate" in {
-    testCompileAs("(defmacro prevent-crash (x) ()) (prevent-crash (+ \"this should crash\"))", Array(
-      Push(Nil),
-      Pop,
-
-      Push(Nil),
-    ))
-  }
-
-  they should "have access to unevaluated version of arguments" in {
-    testCompileAs("(def x 42) (defmacro prevent-crash (x) (builtin/first x)) (prevent-crash (x \"this should crash\"))", Array(
-      Push(42),
-      SetGlobal(0),
-      Pop,
-
-      Push(Nil),
-      Pop,
-
-      GetGlobal(0),
-    ))
-  }
-
-  they should "have be able to return quoted version of args" in {
-    testCompileAs(
-      """
-        (defmacro prevent-crash (x)
-          (builtin/cons 'quote
-            (builtin/cons (builtin/first x)
-              ())))
-
-        (prevent-crash (+ "this should crash"))
-       """,
-      Array(
-        Push(Nil),
-        Pop,
-
-        Push(Symbol("+")),
-      )
     )
   }
 
-  def testCompileAs(str: java.lang.String, instructions: Array[OpCode]): Unit = {
-    val parsed = Parser.run(str).get
-    val compiled = Compiler.compile(parsed)
-    compiled should be(instructions)
+  they should "be expanded when returning literals" in {
+    "(defmacro mac () 42) (mac)".shouldCompileAs(
+      Push(Nil),
+      Pop,
+      Push(42),
+    )
+  }
+
+  they should "be expanded when returning expressions" in {
+    "(def x 42) (defmacro mac () (quote x)) (mac)".shouldCompileAs(
+      Push(42),
+      SetGlobal(0),
+      Pop,
+
+      Push(Nil),
+      Pop,
+
+      GetGlobal(0),
+    )
+  }
+
+  they should "have access to arguments" in {
+    "(defmacro mac (x y) x) (mac 42 1)".shouldCompileAs(
+      Push(Nil),
+      Pop,
+
+      Push(42),
+    )
+  }
+
+  they should "prevent arguments to evaluate" in {
+    "(defmacro prevent-crash (x) ()) (prevent-crash (+ \"this should crash\"))".shouldCompileAs(
+      Push(Nil),
+      Pop,
+
+      Push(Nil),
+    )
+  }
+
+  they should "have access to unevaluated version of arguments" in {
+    "(def x 42) (defmacro prevent-crash (x) (builtin/first x)) (prevent-crash (x \"this should crash\"))".shouldCompileAs(
+      Push(42),
+      SetGlobal(0),
+      Pop,
+
+      Push(Nil),
+      Pop,
+
+      GetGlobal(0),
+    )
+  }
+
+  they should "have be able to return quoted version of args" in {
+    """
+      (defmacro prevent-crash (x)
+        (builtin/cons 'quote
+          (builtin/cons (builtin/first x)
+            ())))
+
+      (prevent-crash (+ "this should crash"))
+     """.shouldCompileAs(
+      Push(Nil),
+      Pop,
+
+      Push(Symbol("+")),
+    )
+  }
+
+
+  implicit class StringAssertions(val source: java.lang.String) {
+    def shouldCompileAs(instructions: OpCode*): Unit = {
+      val parsed = Parser.run(source).get
+      val compiled = new Compiler().compile(parsed)
+      compiled should be(instructions.toArray)
+    }
   }
 }
 
